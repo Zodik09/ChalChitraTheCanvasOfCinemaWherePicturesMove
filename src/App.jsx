60fefd316b61cc77a41a19f963ca3829f39c1ce3
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-05-09 16:54:50 +0530

    Successfully add the Redux Tool Kit for State Management.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementAsync,
+  incrementByAmount,
+} from "./redux/reducers/counterSlice";
 
 const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>
+        Increment by 5
+      </button>
+      <button onClick={() => dispatch(incrementAsync(5))}>
+        Increment by 5 after 2s
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
