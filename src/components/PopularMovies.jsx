61c3b6972909986fd299f1ef0e3420b4d6a5f9d2
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-05-20 12:56:34 +0530

    Remove previously made Proxy Backend Server and add the Cloudflare Workers for TMDB API to work on JIO network.

--- src/components/PopularMovies.jsx
+++ src/components/PopularMovies.jsx
@@ -1,31 +1,41 @@
-import { useEffect, useState } from "react";
-import { fetchFromTMDB } from "../../client/src/api/tmdb";
+import axios from "axios";
+import { useEffect } from "react";
+// import { fetchFromTMDB } from "../../client/src/api/tmdb";
 
 const PopularMovies = () => {
-  const [movies, setMovies] = useState([]);
+  // const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetchFromTMDB("discover/movie", {
-      language: "en-US",
-      include_adult: false,
-      include_video: false,
-      page: 1,
-      sort_by: "popularity.desc",
-    })
-      .then((data) => setMovies(data.results || []))
-      .catch(console.error);
+    const fetchMovies = async () => {
+      try {
+        const res = await axios.get(
+          "https://tmdb-proxy.adarshvishwakarma09nov2k.workers.dev?path=discover/movie",
+          {
+            params: {
+              path: "movie/popular",
+              language: "en",
+            },
+          }
+        );
+        const data = res.data;
+        console.log(data.results);
+      } catch (err) {
+        console.error("Worker fetch failed", err);
+      }
+    };
+    fetchMovies();
   }, []);
 
   return (
     <div>
       <h2>Popular Movies</h2>
-      <ul>
+      {/* <ul>
         {movies.map((movie) => (
           <li key={movie.id}>{movie.id} - {movie.original_title
 }</li>
         ))}
         {console.log(movies)}
-      </ul>
+      </ul> */}
     </div>
   );
 };
