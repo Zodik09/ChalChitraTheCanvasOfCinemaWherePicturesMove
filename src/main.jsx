967871bc0a7f44759003c9c1a7ef724058d1b420
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-05-22 17:38:28 +0530

    Wrap the App with PersistGate and configure PersistGate in store.js to make the already called data Persist so that once called data does not reload on refreshing the page again and again.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // uses localStorage
+import { combineReducers } from "redux";
 import moviesReducer from './counter/moviesSlice'
+import tvShowsReducer from './counter/tvShowsSlice'
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["movies", "tvShows"], // ✅ Only persist these reducers
+};
+
+const rootReducer = combineReducers({
+  movies: moviesReducer,
+  tvShows: tvShowsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    movies: moviesReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // redux-persist needs this
+    }),
+});
+
+export const persistor = persistStore(store);
