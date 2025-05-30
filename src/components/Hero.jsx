96666ce819830dc498d178fa6348fa0b8bf53a4a
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-05-31 00:02:00 +0530

    Display the Heading, Gernes, Languages, Button in Hero.jsx.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,73 +1,71 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import Loading from "./Loading";
-import {
-  fetchNewMovies,
-  fetchNewTVShows,
-  fetchTopRatedMovies,
-  fetchTopRatedTVShows,
-  fetchTrendingAll,
-  fetchTrendingMovies,
-  fetchTrendingTV,
-  fetchUpcomingMovies,
-  fetchNowPlayingMovies,
-  fetchPopularMovies,
-  fetchPopularTVShows,
-  fetchAiringTodayTV,
-} from "../redux/counter/mediaSlice";
+// import Loading from "./Loading";
 
-const Hero = () => {
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import "../../src/index.css";
+
+import { fetchNewMovies, fetchNewTVShows } from "../redux/counter/mediaSlice";
+import {
+  fetchMovieGenres,
+  fetchTVShowGenres,
+  fetchLanguages,
+} from "../redux/counter/metaDataSlice";
+
+const Hero = ({ type = "all" }) => {
   const dispatch = useDispatch();
 
-  // Extracting all media data from Redux state
+  // const fullState = useSelector((state) => state);
+  // console.log(fullState); // Verify the full structure
+
   const {
-    newMovies,
-    newTVShows,
-    topRatedMovies,
-    topRatedTVShows,
-    trendingAll,
-    trendingMovies,
-    trendingTV,
-    upcomingMovies,
-    nowPlayingMovies,
-    popularMovies,
-    popularTVShows,
-    airingTodayTV,
-    isLoading,
-    error,
-  } = useSelector((state) => state.media);
+    newMovies = [],
+    newTVShows = [],
+    isLoading = false,
+    error = null,
+  } = useSelector((state) => state.media || {});
+
+  const {
+    movieGenres = [],
+    tvShowGenres = [],
+    languages = [],
+  } = useSelector((state) => state.metaData || {});
 
   useEffect(() => {
-    // Dispatching all API calls to fetch data
-    dispatch(fetchNewMovies());
-    dispatch(fetchNewTVShows());
-    dispatch(fetchTopRatedMovies());
-    dispatch(fetchTopRatedTVShows());
-    dispatch(fetchTrendingAll());
-    dispatch(fetchTrendingMovies());
-    dispatch(fetchTrendingTV());
-    dispatch(fetchUpcomingMovies());
-    dispatch(fetchNowPlayingMovies());
-    dispatch(fetchPopularMovies());
-    dispatch(fetchPopularTVShows());
-    dispatch(fetchAiringTodayTV());
-  }, [dispatch]);
+    if ((type === "all" || type === "movies") && newMovies.length === 0) {
+      dispatch(fetchNewMovies());
+    }
+    if ((type === "all" || type === "movies") && movieGenres.length === 0) {
+      dispatch(fetchMovieGenres());
+    }
+    if ((type === "all" || type === "tvShows") && newTVShows.length === 0) {
+      dispatch(fetchNewTVShows());
+    }
+    if ((type === "all" || type === "tvShows") && tvShowGenres.length === 0) {
+      dispatch(fetchTVShowGenres());
+    }
+    dispatch(fetchLanguages());
+  }, [
+    dispatch,
+    type,
+    languages.length,
+    newMovies.length,
+    movieGenres.length,
+    newTVShows.length,
+    tvShowGenres.length,
+  ]);
 
-  // Log all fetched data
   useEffect(() => {
     if (!isLoading && !error) {
       console.log("New Movies:", newMovies);
       console.log("New TV Shows:", newTVShows);
-      console.log("Top Rated Movies:", topRatedMovies);
-      console.log("Top Rated TV Shows:", topRatedTVShows);
-      console.log("Trending All:", trendingAll);
-      console.log("Trending Movies:", trendingMovies);
-      console.log("Trending TV:", trendingTV);
-      console.log("Upcoming Movies:", upcomingMovies);
-      console.log("Now Playing Movies:", nowPlayingMovies);
-      console.log("Popular Movies:", popularMovies);
-      console.log("Popular TV Shows:", popularTVShows);
-      console.log("Airing Today TV Shows:", airingTodayTV);
+      console.log("Movie Genres:", movieGenres);
+      console.log("TV Shows Genres:", tvShowGenres);
+      console.log("Languages:", languages);
     } else if (error) {
       console.error("Error fetching data:", error);
     }
@@ -76,49 +74,71 @@ const Hero = () => {
     error,
     newMovies,
     newTVShows,
-    topRatedMovies,
-    topRatedTVShows,
-    trendingAll,
-    trendingMovies,
-    trendingTV,
-    upcomingMovies,
-    nowPlayingMovies,
-    popularMovies,
-    popularTVShows,
-    airingTodayTV,
+    languages,
+    movieGenres,
+    tvShowGenres,
   ]);
 
+  let content = [];
+  if (type === "movies")
+    content = [...newMovies.map((item) => ({ ...item, mediaType: "movie" }))];
+  else if (type === "tvShows")
+    content = [...newTVShows.map((item) => ({ ...item, mediaType: "tvShow" }))];
+  else {
+    content = [
+      ...newMovies.map((item) => ({ ...item, mediaType: "movie" })),
+      ...newTVShows.map((item) => ({ ...item, mediaType: "tvShow" })),
+    ].sort(() => 0.5 - Math.random());
+  }
   return (
-    <div className="hero-container">
-      {/* This is where you'll use the data to display */}
-      <h1>Welcome to ChalChitra!</h1>
-      {isLoading ? (
-        <Loading />
-      ) : error ? (
-        <div className="error">Error: {error}</div>
-      ) : (
-        <div>
-          {/* You can replace these with actual UI components to display data */}
+    <Swiper
+      pagination={{ dynamicBullets: true }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      {content.map((media) => (
+        <SwiperSlide className="relative" key={media.id}>
+          <Link to={`/${media.mediaType}/details/${media.id}`}>
+            <img
+              src={
+                media?.backdrop_path
+                  ? `https://image.tmdb.org/t/p/w500${media.backdrop_path}`
+                  : "https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg"
+              }
+              alt={media?.title || media?.name}
+              className="h-[50vh] w-full object-cover"
+            />
+            <div className="bg-black/50">
+              <h1 className="text-white font-[FontBlack] text-3xl md:text-4xl text-center">
+                {media.original_name || media.original_title}
+              </h1>
+            </div>
+          </Link>
+
           <div>
-            <h2>New Movies</h2>
-            <ul>
-              {newMovies.map((movie) => (
-                <li key={movie.id}>{movie.title}</li>
-              ))}
-            </ul>
+            <span className="bg-black" key={media.original_language}>
+              {languages.find(
+                (lang) => lang.iso_639_1 === media.original_language
+              )?.english_name || media.original_language}
+            </span>
+            {media.genre_ids.map((genreId) => {
+              const genre = movieGenres.find((g) => g.id === genreId);
+              return (
+                <span className="bg-black" key={genreId}>
+                  {genre && genre.name}
+                </span>
+              );
+            })}
           </div>
-          <div>
-            <h2>Trending Movies</h2>
-            <ul>
-              {trendingMovies.map((movie) => (
-                <li key={movie.id}>{movie.title}</li>
-              ))}
-            </ul>
-          </div>
-          {/* Add more sections for other data like Popular TV Shows, Top Rated Movies, etc */}
-        </div>
-      )}
-    </div>
+          <Link
+            to={`/${media.mediaType}/details/${media.id}/trailer`}
+            className="bg-red-400 p-5"
+          >
+            Watch Now
+          </Link>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
