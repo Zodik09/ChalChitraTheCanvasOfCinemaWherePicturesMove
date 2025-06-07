770dd1bb01273df965b3123030cf5d0ca7544992
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-06-07 19:28:45 +0530

    Add the useParams code and comment it for future use.

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+// import { useParams } from 'react-router-dom';
 
 const Trailer = () => {
+  // const params = useParams();
+  // console.log(params);
   return (
     <div>Trailer</div>
   )
